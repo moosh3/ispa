27359b9b61b1fb1e3998bd13b34dab66170398b8
aleccunningham <aleccunningham96@gmail.com>
2017-07-12 21:08:25 -0500

    celery jobs api

--- ispa/core/views.py
+++ ispa/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from .models import Job
+from .serializers import JobSerializer
+
+
+class JobViewSet(mixins.CreateModelMixin,
+                 mixins.ListModelMixin,
+                 mixins.RetrieveModelMixin,
+                 viewsets.GenericViewSet):
+    """
+    API endpoint that allows jobs to be viewed or created.
+    """
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
