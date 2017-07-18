f7bdd57a528ad6e7e0cd185d74d6f1dafc83f375
freddy <freddy_mrqz@yahoo.com>
2017-07-18 18:39:53 -0500

    fixed init.sh

--- init.sh
+++ init.sh
@@ -21,7 +21,7 @@ groupadd docker && usermod -aG docker $USER
 
 #Install python3, python3-dev, python3-setuptools & python3-pip
 
-apt-get install python3 \
+apt-get install -y python3 \
 	python3-dev \
 	python3-setuptools \
 	python3-pip 
