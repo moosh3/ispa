06d867740e5de4b5510a62be702d12e3537ae4c9
aleccunningham <aleccunningham96@gmail.com>
2017-07-21 21:44:27 -0500

    class method to create events

--- ispa_project/events/models/event.py
+++ ispa_project/events/models/event.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
+
 class Event(models.Model):
     
     event_id = models.IntegerField()
     owner = models.ForeignKey(User)
     creation_date = models.DateField('Event Creation Date') 
+    title = models.CharField('Event Name', max_length=45, null=True, blank=True)
     address = models.CharField('Address', max_length=45, null=True, blank=True)
     address2 = models.CharField('Address 2', max_length=45, null=True, blank=True)
     city = models.CharField('City', max_length=45, null=True, blank=True)
@@ -14,6 +16,25 @@ class Event(models.Model):
     zipcode = models.CharField('Zipcode', max_length=10, null=True, blank=True)
     date = models.DateTimeField('Event Date', null=True, blank=True, auto_now=False)
     description = models.CharField('Description', max_length=512, null=True, blank=True) 
+
+    def __str__(self):
+        return 'EVENT {}'.format(self.event_id)
+
+    @classmethod
+    def create_event(cls, owner, title, address, address2,
+                     city, state, zipcode, date, description):
+
+        return cls.object.create(
+            owner=owner,
+            title=title,
+            address=address,
+            address2=address2,
+            city=city,
+            state=state,
+            zipcode=zipcode,
+            date=date,
+            description=description
+        )
 
     class Meta:
         db_table = 'event'
