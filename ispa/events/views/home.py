c25c23f4a4869093f048595e155f043c9b0b27df
Freddy Marquez <freddymrqz28@gmail.com>
2017-07-24 16:52:02 -0500

    home.py conflict fixed

--- ispa/events/views/home.py
+++ ispa/events/views/home.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-from django.views.generic import View
-
-from events.models import Event
-
-class EventDashboard(View):
-    template_name = 'home.html'
-    model = Event
-
-    def dispatch(self, *args, **kwargs):
-        return super(EventDashboard, self).dispatch(*args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super(EventDashboard, self).get_context_data(**kwargs)
-        context['active_events'] = models.Events.objects.filter(
-            is_active=True,
-        )
-        user_created_events = models.EventGuest.objects.filter(
-            event=self.event,
-            guest=self.request.user,
-        ).order_by('-modified')[:5]
-        return context
-=======
 from django.views.generic import FormView
 
 from events.models import Event, EventGuest
@@ -47,4 +24,3 @@ class EventDashboard(FormView):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
         return super(EventDashboard, self).form_valid(form)
->>>>>>> 545c833f37e5ce15bfa5d5fc2a01af6a04b9635f
