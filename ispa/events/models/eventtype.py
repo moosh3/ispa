4170a3d0abc659fe83fd275a2c55e89e6770e926
aleccunningham <aleccunningham96@gmail.com>
2017-07-28 22:17:07 -0500

    categorys for events

--- ispa/events/models/eventtype.py
+++ ispa/events/models/eventtype.py
@@ -10,3 +10,11 @@ class EventType(BaseModel):
     MEETING = 3
     EVENT = 8
     SEMESTER = 5
+
+    EVENT_TYPES = (
+        ('meeting', 'MEETING'),
+        ('event', 'EVENT'),
+        ('semester', 'SEMESTER')
+    )
+
+    category = models.CharField(choices=EVENT_TYPES, default="event", max_length=10)
