d0a1f2a6a46303cb43bde1daf03d702a7b1871a7
aleccunningham <aleccunningham96@gmail.com>
2017-07-29 00:00:53 -0500

    fixed context for active events

--- ispa/events/views/home.py
+++ ispa/events/views/home.py
@@ -16,11 +16,5 @@ class EventDashboard(FormView):
         user_created_events = EventGuest.objects.filter(
             guest=self.request.user,
         ).order_by('-modified')[:5]
-        context['users_events'] = user_created_events
 
         return context
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        return super(EventDashboard, self).form_valid(form)
