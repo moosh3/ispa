2891acf5bb968eeea9c830699ffc9f1327ebc548
aleccunningham <aleccunningham96@gmail.com>
2017-07-30 16:44:52 -0500

    removed EventType model and using constants instead

--- ispa/events/admin.py
+++ ispa/events/admin.py
@@ -2,25 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import Event, Member, EventLocation
+from .models import Event, EventLocation
 
-
-class MemberInline(admin.StackedInline):
-    model = Member
-    can_delete = False
-    verbose_name_plural = 'Member'
-    fk_name = 'user'
-
-class CustomUserAdmin(UserAdmin):
-    inlines = (MemberInline, )
-
-    def get_inline_instances(self, request, obj=None):
-        if not obj:
-            return list()
-        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
-# Register your models here.
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
 
 admin.site.register(Event)
 admin.site.register(EventLocation)
