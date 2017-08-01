55db18accc4fd86986e080eca7ba976d0e270825
aleccunningham <aleccunningham96@gmail.com>
2017-08-01 13:12:01 -0500

    refactored travis script because we dont need docker compose

--- ispa/config/settings/test.py
+++ ispa/config/settings/test.py
@@ -38,6 +38,13 @@ CACHES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # TESTING
 # ------------------------------------------------------------------------------
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
