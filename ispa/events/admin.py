40cf3dc4198c6292a8c90b42199afd4c9e773f12
aleccunningham <aleccunningham96@gmail.com>
2017-08-03 23:16:00 -0500

    register member in admin inline

--- ispa/events/admin.py
+++ ispa/events/admin.py
@@ -2,15 +2,33 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import Event, EventLocation, Owner
+from events import models
 
 class OwnerInline(admin.TabularInline):
-    model = Owner
+    model = models.Owner
 
 class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     inlines = [OwnerInline, ]
 
-admin.site.register(Event, EventAdmin)
-admin.site.register(EventLocation)
-admin.site.register(Owner)
+class MemberInline(admin.StackedInline):
+    model = models.Member
+    can_delete = False
+    verbose_name_plural = 'Member'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (MemberInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+# Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+admin.site.register(models.Event, EventAdmin)
+admin.site.register(models.EventLocation)
+admin.site.register(models.Owner)
