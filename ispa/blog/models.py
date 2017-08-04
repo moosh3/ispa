4095e2e2c47667cada74eb96b4aa706e1aeb2241
Freddy Marquez <freddymrqz28@gmail.com>
2017-08-04 01:40:06 -0500

    added images model

--- ispa/blog/models.py
+++ ispa/blog/models.py
@@ -1,18 +1,24 @@
-from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
-from wagtail.wagtailsearch import index
-
 from django.db import models
+
+from modelcluster.fields import ParentalKey
+
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailsearch import index
 
 # Create your models here.
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(BlogIndexPage, self).get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
 class BlogPage(Page):
     date = models.DateField("Post date")
@@ -28,7 +34,18 @@ class BlogPage(Page):
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
+        InlinePanel('gallery_images', label="Gallery images"),
     ]
 
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
 
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
 
