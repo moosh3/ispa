4da62b773f46dc49cbc48bf513b33ae073e27efa
aleccunningham <aleccunningham96@gmail.com>
2017-08-04 22:53:58 -0500

    created a SymbolDict object and imported factories

--- ispa/events/tests/__init__.py
+++ ispa/events/tests/__init__.py
@@ -5,10 +5,27 @@ from django.contrib.auth.models import User
 
 from events import models as models
 
+from .factories import (
+    UserFactory,
+    EventFactory,
+    EventLocationFactory,
+    AttendeeFactory,
+    OwnerFactory,
+    AttendeeWithEventFactory,
+    OwnerWithEventFactory
+)
+
+
+class SymbolDict(object):
+  def __init__(self, **kwargs):
+    for key in kwargs:
+      setattr(self, key, kwargs[key])
+
 
 class BaseTestCase(TestCase):
 
     def setUp(self):
+
         self.user = User.objects.create(username='test_user')
         self.user.set_password('justtestit')
         self.user.email = 'test_user@example.com'
@@ -24,13 +41,6 @@ class BaseTestCase(TestCase):
         self.location.save()
 
         for count in range(0, 3):
-            u = User.objects.create(username='test_member_{}'.format(count))
-            u.set_password('justtestit')
-            u.email = '{}@example.com'.format(u.username)
-            u.save()
-            models.Member.objects.create(user=u, points=1, bio='')
-
-        for count in range(0, 3):
             u = User.objects.create(username='test_user_{}'.format(count))
             u.set_password('justtestit')
             u.email = '{}@example.com'.format(u.username)
@@ -39,17 +49,24 @@ class BaseTestCase(TestCase):
         self.test_users = User.objects.filter(
             username__startswith='test_user_',
         )
-        self.test_members = models.Member.objects.filter(
-            user__username__startswith='test_member_',
-        )
 
         self.default_event_kwargs = {
             'name': 'Test Event',
             'description': 'Test Event at 1234 Main St.',
+            'owners': self.user,
             'location': self.location,
-            'guests': self.test_members,
-            'date': datetime.date(2007, 12, 5),
+            'guests': self.test_users,
+            'date': datetime.date(2017, 12, 5),
             'points': 5,
             'is_active': True,
-            'eventtype': 'EVENT',
+            'event_type': 'EVENT',
+            'slug': 'test-event'
+        }
+
+        self.default_event_location_kwargs = {
+            'address': '1234 Main St.',
+            'address2': 'Unit 3',
+            'city': 'Chicago',
+            'state': 'IL',
+            'zipcode': '60660',
         }
