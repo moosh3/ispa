bfdc0e607d83cb0fe10e487e839c1f3234c43342
aleccunningham <aleccunningham96@gmail.com>
2017-08-04 22:52:02 -0500

    removed all that weird inline admin stuff

--- ispa/events/admin.py
+++ ispa/events/admin.py
@@ -11,24 +11,7 @@ class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     inlines = [OwnerInline, ]
 
-class MemberInline(admin.StackedInline):
-    model = models.Member
-    can_delete = False
-    verbose_name_plural = 'Member'
-    fk_name = 'user'
-
-class CustomUserAdmin(UserAdmin):
-    inlines = (MemberInline, )
-
-    def get_inline_instances(self, request, obj=None):
-        if not obj:
-            return list()
-        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
-
 # Register your models here.
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
-
 admin.site.register(models.Event, EventAdmin)
 admin.site.register(models.EventLocation)
 admin.site.register(models.Owner)
