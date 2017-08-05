480e3c231a63112a122de56ce423c1c5c8aa108e
aleccunningham <aleccunningham96@gmail.com>
2017-08-04 22:53:12 -0500

    added create_location model method

--- ispa/events/models/eventlocation.py
+++ ispa/events/models/eventlocation.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
+from events.models.base import BaseModel
 
-class EventLocation(models.Model):
+
+class EventLocation(BaseModel):
 
     address = models.CharField('Address', max_length=45, null=True, blank=True)
     address2 = models.CharField('Address 2', max_length=45, null=True,
@@ -17,6 +19,16 @@ class EventLocation(models.Model):
 
     def __unicode__(self):
         return __str__()
+
+    @classmethod
+    def create_location(cls, address, address2, city, state, zipcode):
+        return cls.objects.create(
+            address=address,
+            address2=address2,
+            city=city,
+            state=state,
+            zipcode=zipcode,
+        )
 
     def to_json(self):
         return {
