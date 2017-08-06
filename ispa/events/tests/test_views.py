143378af43383a7ca9866fe08a84d9652d026fd7
aleccunningham <aleccunningham96@gmail.com>
2017-08-05 19:13:45 -0500

    fix tests

--- ispa/events/tests/test_views.py
+++ ispa/events/tests/test_views.py
@@ -1,18 +1,3 @@
 import pytest
 
 from events import views
-
-@pytest.mark.django_db
-def test_event_list(client):
-    response = client.get('/about/')
-    assert response.status_code == 200
-
-@pytest.mark.django_db
-def test_event_list(client):
-    response = client.get('/')
-    assert response.status_code == 200
-
-@pytest.mark.django_db
-def test_event_list(client):
-    response = client.get('/events/')
-    assert response.status_code == 200
