7b68b369558e3bc28928fb8364af884c02da6407
Freddy Marquez <freddymrqz28@gmail.com>
2017-08-08 14:24:41 -0500

    bloggity blog

--- ispa/blog/models.py
+++ ispa/blog/models.py
@@ -1,36 +1,23 @@
 from django.db import models
 
-from modelcluster.fields import ParentalKey
-
-from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
-from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailsearch import index
-
 # Create your models here.
 
 class BlogIndexPage(Page):
+
     intro = RichTextField(blank=True)
 
-    def get_context(self, request):
-        # Update context to include only published posts, ordered by reverse-chron
-        context = super(BlogIndexPage, self).get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
-        context['blogpages'] = blogpages
-        return context
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
 
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
-    
-    def main_image(self):
-        gallery_item = self.gallery_images.first()
-        if gallery_item:
-            return gallery_item.image
-        else:
-            return None
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -41,18 +28,4 @@ class BlogPage(Page):
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
-        InlinePanel('gallery_images', label="Gallery images"),
     ]
-
-class BlogPageGalleryImage(Orderable):
-    page = ParentalKey(BlogPage, related_name='gallery_images')
-    image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
-    )
-    caption = models.CharField(blank=True, max_length=250)
-
-    panels = [
-        ImageChooserPanel('image'),
-        FieldPanel('caption'),
-    ]
-
--- ispa/config/settings/base.py
+++ ispa/config/settings/base.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'events',
     'graphene_django',
     'api',
+    'blog',
 ]
 
 GRAPHENE = {
