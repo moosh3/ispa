2749f3ee894d2295f0c51b8decbb062528392a37
aleccunningham <aleccunningham96@gmail.com>
2017-08-22 08:13:31 -0500

    removed gunicorn logging, put managepy in prod for deployment

--- gunicorn.sh
+++ gunicorn.sh
@@ -6,6 +6,4 @@ python manage.py collectstatic --noinput
 exec gunicorn config.wsgi:application \
   -b 0.0.0.0:8080
   --name ispa \
-  --workers 3 \
-  --log-level=info \
-  --log-file=/srv/logs/access.log
+  --workers 3
