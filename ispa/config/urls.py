9d947e579b8b8cff1d4ef030f1c4b0215ed7bc45
aleccunningham <aleccunningham96@gmail.com>
2017-08-21 20:09:38 -0500

    removed debug_toolbar

--- ispa/config/urls.py
+++ ispa/config/urls.py
@@ -37,8 +37,6 @@ from api.viewsets import (
     SponsorViewSet,
 )
 
-if settings.DEBUG:
-    import debug_toolbar
 
 router = DefaultRouter()
 router.register(r'events', EventViewSet)
@@ -96,9 +94,9 @@ urlpatterns += [
 ]
 
 # django-debug-toolbar
-urlpatterns += [
-    url(r'^__debug__/', include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += [
+#    url(r'^__debug__/', include(debug_toolbar.urls)),
+#] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API
 urlpatterns += [
