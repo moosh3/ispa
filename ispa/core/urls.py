b82ade757089baa8c7cf989c54f9254d5274b307
aleccunningham <aleccunningham96@gmail.com>
2017-08-25 14:10:13 -0500

    lots of edits and changes for paypal dues

--- ispa/core/urls.py
+++ ispa/core/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.conf import settings
 from django.views.generic import TemplateView
 
-from core.views import dues, sponsors
+from core.views import sponsors
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(
@@ -11,7 +11,6 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(
         template_name='ispa/about.html'),
         name='about'),
-    url(r'^dues/$', dues.pay_dues, name='dues'),
     url(r'^sponsors/$', sponsors.list_view, name='list-sponsors'),
     url(r'^sponsor/create/$', sponsors.create_view, name='create-sponsor')
 ]
