2179ae9e5151b8786b5e9dd9fd17f06eaa5bbd1c
aleccunningham <aleccunningham96@gmail.com>
2017-08-25 15:33:47 -0500

    hidden keys

--- ispa/events/views/dues.py
+++ ispa/events/views/dues.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class PayDuesView(TemplateView):
+    # Hidden due to static secret keys
     template_name = 'events/dues.html'
 
 pay_dues = PayDuesView.as_view()
