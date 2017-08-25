166b86c747183c161146686877ac201946637e43
aleccunningham <aleccunningham96@gmail.com>
2017-08-24 22:32:33 -0500

    adding an RSVP form

--- ispa/events/forms/events.py
+++ ispa/events/forms/events.py
@@ -1,5 +1,13 @@
 from django import ModelForm
+from django.contrib.auth.models import User
 from events import models
+
+
+ATTENDING_CHOICES = (
+    ('yes', 'Yes'),
+    ('no', 'No'),
+    ('maybe', 'Maybe'),
+)
 
 class EventForm(ModelForm):
     class Meta:
@@ -13,3 +21,17 @@ class EventForm(ModelForm):
     class Meta:
         model = models.EventLocation
         fields = '__all__'
+
+class RSVPForm(forms.Form):
+    attending = forms.ChoiceField(choices=ATTENDING_CHOICES, initial='no', widget=forms.RadioSelect)
+    email = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super(RSVPForm, self).__init__(*args, **kwargs)
+
+    def save(self):
+        models.Attendance = self.User.objects.get(email=self.cleaned_data['email'])
+
+        Attendance.attending_status = self.cleaned_data['attending']
+        Attendance.save()
+        return Attendance
