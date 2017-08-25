14ea72db9a932ff43edb4071d8834aa33e19223f
aleccunningham <aleccunningham96@gmail.com>
2017-08-25 15:32:40 -0500

    health fix

--- ispa/core/views/sponsors.py
+++ ispa/core/views/sponsors.py
@@ -1,17 +1,18 @@
 from django.db import models
 from django.views.generic import CreateView, ListView
 
+from core.models import Sponsor
 
 class CreateSponsorView(CreateView):
 
-    model = models.Sponsor
+    model = Sponsor
     fields = '__all__'
     success_url = '/events/'
 
 
 class ListSponsorView(ListView):
 
-    model = models.Sponsor
+    model = Sponsor
 
 
 create_view = CreateSponsorView.as_view()
