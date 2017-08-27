7ceca04d83eae851b4f7cc38a5ea948903aeb301
aleccunningham <aleccunningham96@gmail.com>
2017-08-26 19:36:07 -0500

    clean up core ispa templates

--- ispa/core/forms/signup.py
+++ ispa/core/forms/signup.py
@@ -5,7 +5,7 @@ class SignupForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('phone_number', 'tshirt')
-        help_texts = {'username': "firstname.lastname"}
+        placeholders = {'username': "firstname.lastname", 'phone_number': "Phone number"}
 
     def signup(self, request, user):
         user.save()
