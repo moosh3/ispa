41460a754a204105a407fdd923c4c2d27d10e736
aleccunningham <aleccunningham96@gmail.com>
2017-08-26 21:01:23 -0500

    add message model and included template in event detail

---
+++ ispa/events/models/message.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+
+from django.utils.encoding import python_2_unicode_compatible
+from django.db import models
+
+from events.models import BaseModel
+
+
+@python_2_unicode_compatible
+class Message(BaseModel):
+
+    user = models.ForeignKey('auth.User', related_name='messages',)
+    events = models.ForeignKey('Event')
+    text = models.CharField(max_length=1028, blank=True, null=True)
+    title = models.CharField(max_length=512, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('message', args=[self.title])
+
+    def __str__(self):
+        return '{}'.format(self.user.username)
+
+    @property
+    def title(self):
+        return self.__str__()
+
+    @classmethod
+    def create_message(cls, user, event, text, title):
+        return cls.objects.create(
+            user=user,
+            event=event,
+            text=text,
+            title=title,
+        )
