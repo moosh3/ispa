52622c30793e8827f747b252e92ec4844d260ec2
aleccunningham <aleccunningham96@gmail.com>
2017-08-31 09:48:50 -0500

    migration for events

--- run_migrations.sh
+++ run_migrations.sh
@@ -2,5 +2,5 @@
 
 set -c
 
-python manage.py makemigrations --no-input
-python manage.py migration --no-input
+python manage.py makemigrations --no-input --settings=config.settings.production
+python manage.py migration --no-input --settings=config.settings.production
--- start-dev.sh
+++ start-dev.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source ./dev.env
+source dev.env
 
 python manage.py migrate
 python manage.py runserver_plus 0.0.0.0:8000
