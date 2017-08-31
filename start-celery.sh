59a1767375d2c6abd8556191e979069ffe34d05f
aleccunningham <aleccunningham96@gmail.com>
2017-08-30 20:13:37 -0500

    set -e celery, remove path compsoe

--- start-celery.sh
+++ start-celery.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-C_FORCE_ROOT='true' ./manage.py celery worker \
-  -A config.celery \
-  -Q
-  default \
+
+set -e
+
+python manage.py celery worker -A config.celery -Q default \
   --log-file=/var/log/celery.log \
   --loglevel=INFO \
   --concurrency=4
