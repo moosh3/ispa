08941453d5c5ad8d047399a886d03fbc84537050
aleccunningham <aleccunningham96@gmail.com>
2017-08-30 21:02:56 -0500

    exec bash

--- start-celery.sh
+++ start-celery.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-python manage.py celery worker -A config.celery -Q default \
+exec python manage.py celery worker -A config.celery -Q default \
   --log-file=/var/log/celery.log \
   --loglevel=INFO \
   --concurrency=4
